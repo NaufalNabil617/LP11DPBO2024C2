b2acfd703ce035db26c34a82dd841dfaae46aa80
NaufalNabil617 <147363037+NaufalNabil617@users.noreply.github.com>
2024-05-08 20:20:02 +0700

    Add files via upload

---
+++ Program/form.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once("model/Template.class.php");
+include_once("model/DB.class.php");
+include_once("model/Pasien.class.php");
+include_once("model/TabelPasien.class.php");
+include_once("view/FormPasien.php");
+
+
+$tp = new FormPasienView();
+
+if (isset($_POST['add'])) {
+    $tp->addPasien($_POST);
+} else if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $tp->tampilUpdate($id); 
+} else if (isset($_POST['update'])) {
+    $tp->updatePasien($_POST);
+} else {
+    $data = $tp->tampil();
+}
